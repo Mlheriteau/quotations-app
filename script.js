97e92e2ac91ae8d94d5ca25fee0859e85ec9bdb4
Mlherriteau <michel.lheriteau@hotmail.fr>
2025-02-28 12:31:10 +0100

    la loose

---
+++ script.js
@@ -0,0 +1,17 @@
+import { quote-list } from './script.js';
+[
+      {
+        text: "On ne naît pas femme, on le devient.",
+        author: ["Simone de Beauvoir"],
+    }
+]
+
+
+const form = document.getElementById("quote-text");
+
+function quote-form (){
+
+    
+}
+
+
